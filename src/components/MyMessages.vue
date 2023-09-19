001bccebe491dd3cce6653d572d2daaab11cf0df
lstidam2 <lstidam2@ivytech.edu>
2023-09-19 10:27:04 -0400

    start

---
+++ src/components/MyMessages.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h4>Messages</h4>
+        <ul v-for='message in messages'
+        :key="message.id">
+            <li>{{message}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+            messages: ['hello', 'hi', 'its working']
+        }
+    }
+}
+</script>
